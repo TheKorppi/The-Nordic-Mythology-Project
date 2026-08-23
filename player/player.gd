daf1b63ecc778d80d4be36ff55175b1ba65d685f
Wille Soininen <s2312529@edu.savonia.fi>
2026-08-23 20:09:01 +0300

    uusi movement
    pelaajan liikkumisen uudelleenkoodaus

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
-var health = 100
+var speed = 9.0
+var sprint_scalar = 2.0
+var rotation_speed = 1.0
+var health = 100.0
 
 #@onready var stun_timer: Timer = %StunTimer
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	PlayerManager.player = self
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -14,11 +18,36 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)	
 		
-	
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# throw weapon
+		pass
+
 func _physics_process(delta):
-	var speed = 5.5
-	var sprint_scalar = 2.0	
-	var rotation_speed = 0.000000001
+	
+	# Pelaajan liikkuminen
+	# WASD
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1.0
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1.0
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.z -= 1.0
+	
+	if Input.is_action_pressed("move_down"):
+		velocity.z += 1.0
+	
+	# aivan hirveä ehtolause, mutta menkööt
+	# pysäyttää pelaajan kun ei liikuta WASD-näppäimillä
+	if Input.is_action_pressed("move_right") || Input.is_action_pressed("move_left") || Input.is_action_pressed("move_up") || Input.is_action_pressed("move_down"):
+		velocity = velocity.normalized() * speed
+	else:
+		velocity.x = 0
+		velocity.z = 0
 	
 	# Pelaajahahmon kääntyminen
 	
@@ -26,41 +55,18 @@ func _physics_process(delta):
 		var look_dir = Vector2(velocity.x, velocity.z)
 		rotation.y = atan2(-look_dir.x, -look_dir.y)
 	
-	# Pelaajan liikkuminen
-
-	# Juoksu
+	# Juoksu ja hyppy
+	# Näiden pitää olla yllä olevan koodin alla!
 	
-	if Input.is_action_just_pressed("sprint"):
+	if Input.is_action_pressed("sprint"):
 		velocity.x *= sprint_scalar
 		velocity.z *= sprint_scalar
 	
-	# WASD
-	
-	var input_direction2D = Input.get_vector(
-		"move_left", "move_right", "move_up", "move_down"
-		)
-
-		
-	var input_direction3D = Vector3(
-		input_direction2D.x, 0.0, input_direction2D.y
-	)
-	
-	if input_direction3D != Vector3.ZERO:
-		var target_angle = atan2(-input_direction3D.x, -input_direction3D.z)
-		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
-		velocity.x = input_direction3D.x * speed
-		velocity.z = input_direction3D.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = 10
+		velocity.y = 10.0
 	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
-		velocity.y = 0.0
-
-	
-	# Painovoima
+		velocity.y = 0
+		
 	velocity.y -= 20.0 * delta
 	
 	move_and_slide()
