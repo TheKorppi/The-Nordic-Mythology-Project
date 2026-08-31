801f3a25ee7859f96ab524323ce8996a84f70ebb
Tuukka Hänninen <127229132+TheKorppi@users.noreply.github.com>
2026-08-31 14:04:35 +0300

    Revert "aseella hyökkäys"

--- player/weapons/bomb/fire_bomb.gd
+++ player/weapons/bomb/fire_bomb.gd
@@ -20,8 +20,5 @@ func _on_fuse_timeout():
 		if target.is_in_group("Props"):
 			var source = self.global_transform.origin
 			target.Prop_hit(source)
-			
-		if target.is_in_group("enemies"):
-			pass
 	
 	queue_free()
