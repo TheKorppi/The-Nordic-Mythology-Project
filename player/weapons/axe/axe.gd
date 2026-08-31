33e5f4e6f0722cf153a39566a15454e88a1c7202
Korppi <thecyporticguy@gmail.com>
2026-08-31 14:18:28 +0300

    Revert "aseella hyökkäys"
    This reverts commit 122e09266b7f3233aeba5c519f73edaf7981130e.

--- player/weapons/axe/axe.gd
+++
@@ -1,9 +0,0 @@
-extends Area3D
-
-@onready var axe_swing: AnimationPlayer = %AnimationPlayer
-@export var weapon_damage = 40.0
-
-func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("enemies"):
-		body.get_hit(weapon_damage)
-		print("enemy hit")
